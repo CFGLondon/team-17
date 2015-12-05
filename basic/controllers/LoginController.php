8df14d0535c9fe004c4baba7efd13e7fe7661026
James Thompson <thompsonjamesrichard@gmail.com>
2015-12-05 02:25:35 +0000

    login attempts

--- basic/controllers/LoginController.php
+++ basic/controllers/LoginController.php
@@ -9,36 +9,53 @@ use yii\helpers\Html;
 
 class LoginController extends Controller
 {
+	private $clientId = '229XBT';
+	
 	public function actionTry()
 	{
-		$session = \Yii::$app->session;
 		$request = \Yii::$app->request;
 		
 		$clientId = '229XBT';
 		$responseType = 'code';
 		$scope = Html::encode('activity profile');
 		$prompt = 'login';
-		
-		if ($session->get('try') == "loggedIn") {
-			$session->set('try', 'reset');
-			$session->set('dump', $request->post());
-			$this->redirect('@web/index.php?r=site/dump');
-		}
 			
-		else {
-			$session->set('try', "loggedIn");
-			
-			return $this->redirect(
-					"https://www.fitbit.com/oauth2/authorize?"
-					."response_type=$responseType&"
-					."client_id=$clientId&"
-					."scope=$scope"
-			);
-		}		
+		return $this->redirect(
+				"https://www.fitbit.com/oauth2/authorize?"
+				."response_type=$responseType&"
+				."client_id=$this->clientId&"
+				."scope=$scope"
+		);	
 	}
 	
 	public function actionSuccess()
 	{
-		return $this->render('success');
+		$session = \Yii::$app->session;
+		$request = \Yii::$app->request;
+		
+		$base64auth = base64_encode("229XBT:fd590071df7af3a872eec8d61d80f35e");
+		echo $base64auth;
+		$url = 'https://api.fitbit.com/oauth2/token';
+		$data = array(
+			'client_id' => $this->clientId,
+			'grant_type' => 'authorization_code',
+			'redirect_uri' => Yii::getAlias('@web/index.php?r=login/success')."&code=".$request->get('code')
+		);
+		$options = array(
+				'https' => array(
+						'header'  => "Authorization: Basic ".$base64auth."\r\n".
+									 "Content-type: application/x-www-form-urlencoded\r\n",
+						'method'  => 'POST',
+						'content' => http_build_query($data),
+				),
+		);
+		$context  = stream_context_create($options);
+		$result = file_get_contents($url, false, $context);
+		
+		$session->set('dump', $result);
+		return $this->redirect('@web/index.php?r=site/dump');
+		
+		//$session->set('dump', $request->post());
+		//return $this->redirect('@web/index.php?r=site/dump');
 	}
 }
